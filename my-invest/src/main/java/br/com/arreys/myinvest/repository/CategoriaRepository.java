030ddeecfc3bf866a13b5c6403f303a5d63c844e
Arthur Alves <art4321@outlook.com.br>
2021-06-01 23:15:18 -0300

    /

--- my-invest/src/main/java/br/com/arreys/myinvest/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.arreys.myinvest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.arreys.myinvest.domain.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-    
-}
